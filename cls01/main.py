39a0a572fbbb41d259d6d904fdb466a7be566e04
zdwweb <15175529059@163.com>
2017-10-16 08:08:29 +0800

    Create main.py

---
+++ cls01/main.py
@@ -0,0 +1,18 @@
+import re
+
+ptn = re.compile(r'[a-zA-Z0-9]+@[a-z0-9]+\.[a-z]{2,3}|\d{11}')
+
+
+def matching(string):
+    message = ptn.findall(string)
+    print(message)
+
+
+def main():
+    txt = '''我的电子邮件tom@gmail.com.
+             将什么发送到jerry123@163.com或者3123432@qq.com.
+             若遇特殊情况打电话给18212344567.'''
+    matching(txt)
+
+
+main()
